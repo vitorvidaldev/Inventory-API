4e7e22017650ba1bd47ecc1788b35aefd1bacf7f
Vitor Vidal <vitorvidal.dev@gmail.com>
2022-04-18 21:57:15 -0300

    Adding stock methods

--- src/main/kotlin/dev/vitorvidal/inventory/api/application/controller/StockController.kt
+++ src/main/kotlin/dev/vitorvidal/inventory/api/application/controller/StockController.kt
@@ -1,9 +1,28 @@
 package dev.vitorvidal.inventory.api.application.controller
 
 import dev.vitorvidal.inventory.api.application.service.StockService
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import dev.vitorvidal.inventory.api.domain.vo.stock.AddStockVO
+import dev.vitorvidal.inventory.api.domain.vo.stock.StockVO
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import java.util.*
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/rest/v1/stocks")
-class StockController(stockService: StockService)
+class StockController(val stockService: StockService) {
+
+    @GetMapping("/{productId}")
+    fun getCurrentProductStock(@PathVariable(name = "productId") productId: UUID): ResponseEntity<StockVO> {
+        val stockVO: StockVO = stockService.getCurrentProductStock(productId)
+        return ResponseEntity.ok().body(stockVO)
+    }
+
+    // TODO post vs put vs patch
+    @PostMapping
+    fun addProductStock(@RequestBody @Valid addStockVO: AddStockVO): ResponseEntity<StockVO> {
+        val stockVO: StockVO = stockService.addProductStock(addStockVO)
+        return ResponseEntity.status(HttpStatus.CREATED).body(stockVO)
+    }
+}
--- src/main/kotlin/dev/vitorvidal/inventory/api/application/service/StockService.kt
+++ src/main/kotlin/dev/vitorvidal/inventory/api/application/service/StockService.kt
@@ -1,7 +1,17 @@
 package dev.vitorvidal.inventory.api.application.service
 
 import dev.vitorvidal.inventory.api.domain.repository.StockRepository
+import dev.vitorvidal.inventory.api.domain.vo.stock.StockVO
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
-class StockService(stockRepository: StockRepository)
+class StockService(stockRepository: StockRepository) {
+    fun getCurrentProductStock(productId: UUID): StockVO {
+        TODO("Not yet implemented")
+    }
+
+    fun addProductStock(addStockVO: Any): StockVO {
+        TODO("Not yet implemented")
+    }
+}
