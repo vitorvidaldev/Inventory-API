2d6297d6cdc8319f88c9fe98cfd4f689443ce2a5
Vitor Vidal <vitorvidal.dev@gmail.com>
2022-05-22 08:39:28 -0300

    Finishing sale implementation

--- src/main/kotlin/dev/vitorvidal/inventory/api/application/service/SaleService.kt
+++ src/main/kotlin/dev/vitorvidal/inventory/api/application/service/SaleService.kt
@@ -1,23 +1,39 @@
 package dev.vitorvidal.inventory.api.application.service
 
+import dev.vitorvidal.inventory.api.domain.entity.SaleEntity
 import dev.vitorvidal.inventory.api.domain.repository.SaleRepository
 import dev.vitorvidal.inventory.api.domain.vo.sale.SaleVO
 import dev.vitorvidal.inventory.api.domain.vo.sale.SellVO
 import org.springframework.data.domain.Page
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
+import org.springframework.web.server.ResponseStatusException
 import java.util.*
 
 @Service
 class SaleService(val saleRepository: SaleRepository) {
     fun getSaleData(saleId: UUID): SaleVO {
-        TODO("Not yet implemented")
+        val optionalSale = saleRepository.findById(saleId)
+        if (optionalSale.isPresent) {
+            val sale = optionalSale.get()
+            return SaleVO(
+                sale.saleId,
+                sale.productId,
+                sale.buyerId
+            )
+        }
+        throw ResponseStatusException(HttpStatus.NOT_FOUND, "Error getting sale data")
     }
 
     fun getUserPurchaseHistory(userId: UUID): Page<SaleVO> {
-        TODO("Not yet implemented")
+        val buyerPurchaseData = saleRepository.findByBuyerId(userId)
+        return buyerPurchaseData.map { data ->
+            SaleVO(data.saleId, data.productId, data.buyerId)
+        }
     }
 
     fun sell(sellVO: SellVO): SaleVO {
-        TODO("Not yet implemented")
+        val saleEntity = saleRepository.save(SaleEntity(sellVO.userId, sellVO.productId))
+        return SaleVO(saleEntity.saleId, saleEntity.productId, saleEntity.buyerId)
     }
 }
--- src/test/kotlin/dev/vitorvidal/inventory/api/application/service/SaleServiceTest.kt
+++ src/test/kotlin/dev/vitorvidal/inventory/api/application/service/SaleServiceTest.kt
@@ -1,7 +1,118 @@
 package dev.vitorvidal.inventory.api.application.service
 
+import dev.vitorvidal.inventory.api.domain.entity.SaleEntity
+import dev.vitorvidal.inventory.api.domain.repository.SaleRepository
+import dev.vitorvidal.inventory.api.domain.vo.sale.SellVO
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
+import org.mockito.kotlin.any
+import org.mockito.kotlin.verify
+import org.springframework.data.domain.PageImpl
+import org.springframework.http.HttpStatus
 import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.web.server.ResponseStatusException
+import java.util.*
 
 @ExtendWith(SpringExtension::class)
-internal class SaleServiceTest
+internal class SaleServiceTest {
+    @InjectMocks
+    lateinit var saleService: SaleService
+
+    @Mock
+    lateinit var saleRepository: SaleRepository
+
+    @Test
+    fun shouldGetSaleCorrectly() {
+        val saleIdMock = UUID.randomUUID()
+        val userIdMock = UUID.randomUUID()
+        val productIdMock = UUID.randomUUID()
+        val saleEntityMock = mock(SaleEntity::class.java)
+
+        `when`(saleRepository.findById(saleIdMock)).thenReturn(Optional.of(saleEntityMock))
+
+        `when`(saleEntityMock.saleId).thenReturn(saleIdMock)
+        `when`(saleEntityMock.buyerId).thenReturn(userIdMock)
+        `when`(saleEntityMock.productId).thenReturn(productIdMock)
+
+        val saleVO = saleService.getSaleData(saleIdMock)
+
+        assertNotNull(saleVO)
+        assertEquals(saleIdMock, saleVO.saleId)
+        assertEquals(userIdMock, saleVO.buyerId)
+        assertEquals(productIdMock, saleVO.productId)
+
+        verify(saleRepository).findById(saleIdMock)
+    }
+
+    @Test
+    fun shouldThrowNotFoundExceptionGettingSaleData() {
+        val saleIdMock = UUID.randomUUID()
+
+        `when`(saleRepository.findById(saleIdMock)).thenReturn(Optional.empty())
+
+        val exception = assertThrows(ResponseStatusException::class.java) {
+            saleService.getSaleData(saleIdMock)
+        }
+
+        assertNotNull(exception)
+        assertEquals(HttpStatus.NOT_FOUND, exception.status)
+        assertEquals("Error getting sale data", exception.reason)
+
+        verify(saleRepository).findById(saleIdMock)
+    }
+
+    @Test
+    fun shouldGetPurchaseHistoryCorrectly() {
+        val userIdMock = UUID.randomUUID()
+        val saleIdMock = UUID.randomUUID()
+        val productIdMock = UUID.randomUUID()
+        val saleEntityMock = mock(SaleEntity::class.java)
+
+        `when`(saleRepository.findByBuyerId(userIdMock)).thenReturn(PageImpl(listOf(saleEntityMock)))
+
+        `when`(saleEntityMock.saleId).thenReturn(saleIdMock)
+        `when`(saleEntityMock.buyerId).thenReturn(userIdMock)
+        `when`(saleEntityMock.productId).thenReturn(productIdMock)
+
+        val purchaseHistory = saleService.getUserPurchaseHistory(userIdMock)
+
+        assertNotNull(purchaseHistory)
+        assertEquals(saleIdMock, purchaseHistory.content[0].saleId)
+        assertEquals(userIdMock, purchaseHistory.content[0].buyerId)
+        assertEquals(productIdMock, purchaseHistory.content[0].productId)
+
+        verify(saleRepository).findByBuyerId(userIdMock)
+    }
+
+    @Test
+    fun shouldSellCorrectly() {
+        val userIdMock = UUID.randomUUID()
+        val saleIdMock = UUID.randomUUID()
+        val productIdMock = UUID.randomUUID()
+        val saleEntityMock = mock(SaleEntity::class.java)
+        val sellVOMock = mock(SellVO::class.java)
+
+        `when`(saleEntityMock.saleId).thenReturn(saleIdMock)
+        `when`(saleEntityMock.buyerId).thenReturn(userIdMock)
+        `when`(saleEntityMock.productId).thenReturn(productIdMock)
+
+        `when`(sellVOMock.productId).thenReturn(productIdMock)
+        `when`(sellVOMock.userId).thenReturn(userIdMock)
+
+        `when`(saleRepository.save(any())).thenReturn(saleEntityMock)
+
+        val saleVO = saleService.sell(sellVOMock)
+
+        assertNotNull(saleVO)
+        assertEquals(saleIdMock, saleVO.saleId)
+        assertEquals(userIdMock, saleVO.buyerId)
+        assertEquals(productIdMock, saleVO.productId)
+
+        verify(saleRepository).save(any())
+    }
+}
