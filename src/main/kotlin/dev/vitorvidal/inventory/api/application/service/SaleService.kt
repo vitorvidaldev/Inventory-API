7b5f2ea8f428a6014e2c401f0bef9436ae43fe70
Vitor Vidal <vitorvidal.dev@gmail.com>
2022-05-21 21:43:07 -0300

    Initial sale implementation

--- src/main/kotlin/dev/vitorvidal/inventory/api/application/service/SaleService.kt
+++ src/main/kotlin/dev/vitorvidal/inventory/api/application/service/SaleService.kt
@@ -1,6 +1,23 @@
 package dev.vitorvidal.inventory.api.application.service
 
+import dev.vitorvidal.inventory.api.domain.repository.SaleRepository
+import dev.vitorvidal.inventory.api.domain.vo.sale.SaleVO
+import dev.vitorvidal.inventory.api.domain.vo.sale.SellVO
+import org.springframework.data.domain.Page
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
-class SaleService(val saleReportService: ReportService)
+class SaleService(val saleRepository: SaleRepository) {
+    fun getSaleData(saleId: UUID): SaleVO {
+        TODO("Not yet implemented")
+    }
+
+    fun getUserPurchaseHistory(userId: UUID): Page<SaleVO> {
+        TODO("Not yet implemented")
+    }
+
+    fun sell(sellVO: SellVO): SaleVO {
+        TODO("Not yet implemented")
+    }
+}
---
+++ src/main/kotlin/dev/vitorvidal/inventory/api/domain/vo/sale/SellVO.kt
@@ -0,0 +1,5 @@
+package dev.vitorvidal.inventory.api.domain.vo.sale
+
+import java.util.*
+
+class SellVO(val userId: UUID, val productId: UUID)
